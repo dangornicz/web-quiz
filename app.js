5cbd00fc30ddae83f0d795df4092a8e4c08462d2
dangornicz <dangornicz@gmail.com>
2023-09-19 17:16:26 -0400

    logic, color, correct answer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let data, ansArr, display, random, q, a1 ,a2 ,a3, a4;
 let qNum = 0;
 let numCorrect = 0;
+let green = "rgb(41, 177, 41)";
 
 async function callQuiz() {
     
@@ -23,15 +24,14 @@ window.onload = async () => {
     a2 = document.getElementById("l2");
     a3 = document.getElementById("l3");
     a4 = document.getElementById("l4");
-    ansArr = [a1 , a2 , a3 , a4];
-    
+    ansArr = [a1 , a2 , a3 , a4]; //array that holds the answers for current question
+
     await callQuiz();
     assignments(); 
     
 };
 
-
-function assignments() {
+function assignments() { //assign question and answers to html elements
     let temp = qNum + 1;
     q.innerHTML = `${temp}. ${data.results[qNum].question}`;
 
@@ -48,42 +48,48 @@ function assignments() {
     }
 }  
 
-function checkAnswer() {
+function checkAnswer() { //checks radio button selected vs. correct answer and clears radio buttons 
     const radioSelected = document.querySelector('input[name="btn"]:checked');
-    
-    if(qNum < 9) {
-        
-        if(radioSelected != null) {
-            if(radioSelected.value == random) {
+
+        if(radioSelected != null) { //if radio button is selected (else displays message)
+            if(radioSelected.value == random) { //if answer correct 
                 numCorrect++;
+                display.style.color = green;
+                display.style.borderColor = green;
+                display.innerHTML = `Score = ${numCorrect} / 10`;
+            } else { //if answer incorrect
+                display.style.color = "red";
+                display.style.borderColor = "red";
+                display.innerHTML = `Score = ${numCorrect} / 10<br>The correct answer was: ${data.results[qNum].correct_answer}`;
             }
-            display.innerHTML = `Score = ${numCorrect} / 10`;
-            qNum++;
+            qNum++; //enumerate variable that tracks what # question 
         } else {
             alert("Please select an answer!");
         }
+        radioSelected.checked = false; //clear radio buttons
     }
-    radioSelected.checked = false;
     
-}
+    
 
-async function btnClick() {
-    if(qNum < 9){
-       
+
+async function btnClick() {//when button is clicked
+
+    if(qNum < 9){ //for questions 1-9 (indexes 0-8 in array)
         checkAnswer();
         assignments();
-         
-    } else if(qNum == 9) {
-        qNum = 10; //to trigger 'else' below (qNum will be reset to '0' in 'else')
-        numCorrect = 0;
+    } else if(qNum == 9) {//after question #10 is answered
         checkAnswer(); //to reset radio buttons
+        qNum = 10; //to trigger 'else' below (qNum will be reset to '0' in 'else')
         q.innerHTML = "GAME OVER!!! Press button below to play again";
         submit.value = "Click to play again!";
-        ansArr.forEach(element => {
+        ansArr.forEach(element => { //clear text-values in radio buttons
             element.innerHTML = ""; 
         });
-    } else {
+    } else {//when button clicked to reset game
         qNum = 0;
+        numCorrect = 0;
+        display.style.color = green;
+        display.style.borderColor = green;
         display.innerHTML = `Score = ${numCorrect} / 10`;
         submit.value = "Click to answer";
         await callQuiz();
